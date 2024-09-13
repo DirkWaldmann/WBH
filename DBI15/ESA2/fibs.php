b5719e4ace9de2d4fedbf20d4a5147030a4a92e4
DirkWaldmann <Dirk.Waldmann79@gmx.de>
2024-09-13 23:45:08 +0200

    Update fibs.php

--- DBI15/ESA2/fibs.php
+++ DBI15/ESA2/fibs.php
@@ -1,18 +1,5 @@
 <?php
-setUrl('https://wbhdbi15-f1db.restdb.io/rest/test-001');
-$request->setMethod(HTTP_METH_GET);
 
-$request->setHeaders(array(
-  'cache-control' => 'no-cache',
-  'x-apikey' => '9a5654028784702a87bc8c3d05602cf429886',
-  'content-type' => 'application/json'
-));
+echo "<b>Hello</b>"
 
-try {
-  $response = $request->send();
-
-  echo $response->getBody();
-} catch (HttpException $ex) {
-  echo $ex;
-}
 ?>
