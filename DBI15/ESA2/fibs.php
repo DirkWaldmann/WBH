32f1248a6f5502922d6e851285c7281c4ed524a4
DirkWaldmann <Dirk.Waldmann79@gmx.de>
2024-09-13 23:23:43 +0200

    Update fibs.php

--- DBI15/ESA2/fibs.php
+++ DBI15/ESA2/fibs.php
@@ -1,27 +1,18 @@
 <?php
-header('Content-Type:˽text/event-stream');
-header('Cache-Control: no-cache'); // ist zu empfehlen
-function sendeDaten($i , $res) {
-echo "data: ", "fib(", $i, ")=", $res, "\n\n";
-echo PHP_EOL;
-ob_flush();
-flush();
+setUrl('https://wbhdbi15-f1db.restdb.io/rest/test-001');
+$request->setMethod(HTTP_METH_GET);
+
+$request->setHeaders(array(
+  'cache-control' => 'no-cache',
+  'x-apikey' => '9a5654028784702a87bc8c3d05602cf429886',
+  'content-type' => 'application/json'
+));
+
+try {
+  $response = $request->send();
+
+  echo $response->getBody();
+} catch (HttpException $ex) {
+  echo $ex;
 }
-function fibonacci($n) {
-if ($n < 2)
-return 1;
-return fibonacci($n-1) + fibonacci($n-2);
-}
-$i = 10;
-$start = time();
-echo "data:˽","Neue˽HTTP-Verbindung˽eingerichtet","\n\n";
-ob_flush();
-flush();
-do {
-// Nach 5 Sekunden wird die Verbindung rekonstruiert.
-if ((time() – $start) > 5) { die(); }
-sendeDaten($i, fibonacci($i));
-$i += 1;
-sleep(1);
-} while(true);
 ?>
