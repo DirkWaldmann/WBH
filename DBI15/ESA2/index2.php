6f9459d0c3addc75962eaca45a9dd5d890654f16
DirkWaldmann <Dirk.Waldmann79@gmx.de>
2024-09-13 23:52:41 +0200

    Update index2.php

--- DBI15/ESA2/index2.php
+++ DBI15/ESA2/index2.php
@@ -1,17 +1,22 @@
+<!doctype html>
+<html>
 
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>DBI15 ESA-2</title> 
-  </head>
+   <head>
+    <meta charset=utf-8>
+    <title>Umfrage</title>
+   </head>
 
-  <body>
-	<h1>DBI15 Einsendeaufgabe 2a</h1>
-	<p>Technologie: HTML und JavaScript</p>
-
-  <?php include "fibs.php";?>
-
-    
-  </body>
+   <body>
+	 <h1>Umfrage zum Studium</h1>
+	 <p>Bitte gib deinen Namen ein oder lasse das Feld Name frei und du nimmst anonym an der Umfrage teil.</p>
+	
+     <form method="get" action="survey.php">
+		<label for="name">Dein Name</label>
+		<input name="name" type="text">
+        <input type="submit" value="Umfrage starten">
+     </form>
+     <br>
+   </body>
+   
 </html>
+
